42e23a6b3588fcafe3b631e72f6d18c4936a2ca8
imthekuni <imthekuni@gmail.com>
2016-07-26 07:20:37 -0700

    re-install icaclient

--- files/install/install.sh
+++ files/install/install.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
 apt-get update
-#cd /tmp
-#wget http://www.vinnymac.org/downloads/citrix/icaclient-mod-ubuntu-14-04.deb
-#sudo dpkg -i icaclient-mod-ubuntu-14-04.deb
-#sudo apt-get -f -y install
-#sudo ln -s /usr/share/ca-certificates/mozilla/* /opt/Citrix/ICAClient/keystore/cacerts/
 
 # Install chromium browser
-#sudo apt-get install gconf-service libgconf-2-4 libatk1.0-0 libgtk2.0-0 libnspr4 libnss3 libxss1 libnss3 xdg-utils chromium-codecs-ffmpeg-extra chromium-codecs-ffmpeg chromium-browser-l10n
 sudo apt-get install -y chromium-browser
-#sudo apt-get -f -y install
-#sudo apt-get install chromium-browser
 
+# Install ICAClient
+cd /tmp
+wget http://www.vinnymac.org/downloads/citrix/icaclient-mod-ubuntu-14-04.deb
+sudo dpkg -i icaclient-mod-ubuntu-14-04.deb
+sudo apt-get -f -y install
+sudo ln -s /usr/share/ca-certificates/mozilla/* /opt/Citrix/ICAClient/keystore/cacerts/
 
+# Set startup apps (e.g., chromium)
 cp /tmp/install/startapp.sh /startapp.sh
 chmod +x /startapp.sh
+
+# Clean up
+sudo apt-get autoremove
