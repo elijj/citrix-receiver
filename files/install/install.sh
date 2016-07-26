720365e0b450c203f966415f07615798ea7fa344
imthekuni <imthekuni@gmail.com>
2016-07-26 05:24:15 -0700

    install chromium browser and auto-start app

--- files/install/install.sh
+++ files/install/install.sh
@@ -6,3 +6,8 @@ wget http://www.vinnymac.org/downloads/citrix/icaclient-mod-ubuntu-14-04.deb
 sudo dpkg -i icaclient-mod-ubuntu-14-04.deb
 sudo apt-get install -f
 sudo ln -s /usr/share/ca-certificates/mozilla/* /opt/Citrix/ICAClient/keystore/cacerts/
+
+sudo apt-get install chromium browser
+
+cp /tmp/install/startapp.sh /startapp.sh
+chmod +x /startapp.sh
