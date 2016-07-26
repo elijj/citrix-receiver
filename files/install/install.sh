923faea8cb17b73524ce43cd83285a38e377efd6
imthekuni <imthekuni@gmail.com>
2016-07-26 06:55:38 -0700

    remove icaclient install for now

--- files/install/install.sh
+++ files/install/install.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 apt-get update
-cd /tmp
-wget http://www.vinnymac.org/downloads/citrix/icaclient-mod-ubuntu-14-04.deb
-sudo dpkg -i icaclient-mod-ubuntu-14-04.deb
-sudo apt-get -f -y install
-sudo dpkg -i icaclient-mod-ubuntu-14-04.deb
-sudo ln -s /usr/share/ca-certificates/mozilla/* /opt/Citrix/ICAClient/keystore/cacerts/
+#cd /tmp
+#wget http://www.vinnymac.org/downloads/citrix/icaclient-mod-ubuntu-14-04.deb
+#sudo dpkg -i icaclient-mod-ubuntu-14-04.deb
+#sudo apt-get -f -y install
+#sudo ln -s /usr/share/ca-certificates/mozilla/* /opt/Citrix/ICAClient/keystore/cacerts/
 
 #sudo apt-get install gconf-service libgconf-2-4 libatk1.0-0 libgtk2.0-0 libnspr4 libnss3 libxss1 libnss3 xdg-utils chromium-codecs-ffmpeg-extra chromium-codecs-ffmpeg chromium-browser-l10n
 sudo apt-get install chromium-browser
