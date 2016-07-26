1fea31cb67c0d549be7fbaa34eb56d9a524e56ee
imthekuni <imthekuni@gmail.com>
2016-07-26 08:27:32 -0700

    install gdebi

--- files/install/install.sh
+++ files/install/install.sh
@@ -6,10 +6,12 @@ apt-get update
 sudo apt-get install -y chromium-browser
 
 # Install ICAClient
+sudo apt-get install -f -y gdebi
 cd /tmp
 wget http://www.vinnymac.org/downloads/citrix/icaclient-mod-ubuntu-14-04.deb
-sudo dpkg -i icaclient-mod-ubuntu-14-04.deb
-sudo apt-get -f -y install
+#sudo dpkg -i icaclient-mod-ubuntu-14-04.deb
+sudo gdebi icaclient-mod-ubuntu-14-04.deb
+#sudo apt-get -f -y install
 #sudo ln -s /usr/share/ca-certificates/mozilla/* /opt/Citrix/ICAClient/keystore/cacerts/
 
 # Set startup apps (e.g., chromium)
