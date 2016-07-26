04eed727c918166ad83d850756b05f23e7bdb28a
imthekuni <imthekuni@gmail.com>
2016-07-26 10:32:14 -0700

    update

--- files/install/install.sh
+++ files/install/install.sh
@@ -24,8 +24,8 @@
 cd $HOME
 mv $HOME/.ICAClient $HOME/.ICAClient_save
 sudo add-apt-repository universe
-sudo apt-get install -f -y apt-file
-sudo apt-get -f -y install git
+sudo apt-get install -y apt-file
+sudo apt-get -y install git
 sudo dpkg -P icaclient
 rm -rf foo
 sudo dpkg --add-architecture i386 # only needed once
