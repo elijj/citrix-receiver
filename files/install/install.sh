929b22b0d91e2571ac6624b635dee3b654c485d1
imthekuni <imthekuni@gmail.com>
2016-07-26 06:16:06 -0700

    install after getting dependencies

--- files/install/install.sh
+++ files/install/install.sh
@@ -5,10 +5,13 @@ cd /tmp
 wget http://www.vinnymac.org/downloads/citrix/icaclient-mod-ubuntu-14-04.deb
 sudo dpkg -i icaclient-mod-ubuntu-14-04.deb
 sudo apt-get install -f
+sudo dpkg -i icaclient-mod-ubuntu-14-04.deb
 sudo ln -s /usr/share/ca-certificates/mozilla/* /opt/Citrix/ICAClient/keystore/cacerts/
 
 sudo apt-get install chromium-browser
 sudo apt-get -f install
+sudo apt-get install chromium-browser
+
 
 cp /tmp/install/startapp.sh /startapp.sh
 chmod +x /startapp.sh
